f0edd5c74cdb34520acc400ac26152513ba4afba
Pei-Hua Wang <31009954+wangpeihua1231@users.noreply.github.com>
2025-02-25 17:59:29 +0800

    Create statevector_pennylane.py
    For Figure 2b

---
+++ statevector_pennylane.py
@@ -0,0 +1,12 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+dev = qml.device("default.qubit", wires=2)
+
+@qml.qnode(dev)
+def circuit():
+    qml.Hadamard(wires=0)
+    qml.CNOT(wires=[0, 1])
+    return qml.state()
+
+circuit()
